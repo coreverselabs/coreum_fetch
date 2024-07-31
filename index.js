0562ba878c1bccef8c69a61ab9bac2eb0e9cc27d
harshnambiar <harrisonlvl2@gmail.com>
2024-07-31 11:32:58 +0530

    added nft check method

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import * as cheerio from "cheerio";
 
 const add = 'core1x9hd9r7duv2gagztvvqlw94v5gy4zd9x5f7kl9';//'core1zn2ns3ls68jlsv5dgkuz0rxsxt5fhk7n9cfl23';
 
-//const add = '0xb5b011030eacc4f6994d35d84ff6cf936a7d4fdc17cfe9f0a554246bced32852';
+const add2 = 'core1zgdprlr3hz5hhke9ght8mq723a8wlnzqwd60hm';
+
+const nft = 'mc-core1zgdprlr3hz5hhke9ght8mq723a8wlnzqwd60hm';
 
 async function fetch_bal(add){
     const url = 'https://full-node.mainnet-1.coreum.dev:1317/coreum/asset/ft/v1/accounts/'.concat(add).concat('/balances/summary/ucore');
@@ -17,8 +19,23 @@ async function fetch_bal(add){
         console.log(e);
     }
     
+}
+
+
+async function fetch_nft(add, nft){
+    const url = 'https://full-node.mainnet-1.coreum.dev:1317/coreum/nft/v1beta1/balance/'.concat(add).concat('/').concat(nft);
+     try {
+        const response = await axios.get(url);
+
+         console.log(response.data.amount);
+
     }
+    catch (e){
+        console.log(e);
+    }
+
+}
 
 
 await fetch_bal(add);
-
+await fetch_nft(add2, nft);
